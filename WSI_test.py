b97492f0d28de3cf3d14f1db4d0508ba6c733129
hustwl159 <hustwl159@qq.com>
2024-10-03 21:13:02 +0800

    2024年10月3日_2

--- WSI_test.py
+++ WSI_test.py
@@ -1,3 +1,5 @@
 '''
 视觉模型、映射结构、语言模型、结构化模型
+PreFLMR: Scaling Up Fine-Grained Late-Interaction Multi-modal Retrievers——框架图
+Richer fusion network for breast cancer classification based on multimodal data——WSI+结构化数据
 '''
